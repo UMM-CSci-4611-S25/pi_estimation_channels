0f0766f79114d5e80f422a291bfea818a3d379f7
Nic McPhee <mcphee@morris.umn.edu>
2025-04-22 16:20:30 -0500

    Implement single-generator/single-classifier system

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ use std::{
     thread,
 };
 
-struct Sender<T> {
+struct MessageGenerator<T> {
     num_messages: u32,
     send_channel: SyncSender<T>,
 }
 
-impl<T> Sender<T>
+impl<T> MessageGenerator<T>
 where
     T: Default + Debug,
     StandardUniform: Distribution<T>,
@@ -29,25 +29,21 @@ where
         }
     }
 
-    pub fn send_stuff(&self) {
+    pub fn send_messages(&self) {
         for _ in 0..self.num_messages {
             let value = rng().random();
-            println!("About to send {value:?}.");
             self.send_channel.send(value).unwrap();
         }
     }
 }
 
-struct Receiver<T> {
+struct PointManager {
     num_messages: u32,
-    receive_channel: mpsc::Receiver<T>,
+    receive_channel: mpsc::Receiver<Point>,
 }
 
-impl<T> Receiver<T>
-where
-    T: Debug,
-{
-    pub fn new(num_messages: u32, receive_channel: mpsc::Receiver<T>) -> Self {
+impl PointManager {
+    pub fn new(num_messages: u32, receive_channel: mpsc::Receiver<Point>) -> Self {
         Self {
             num_messages,
             receive_channel,
@@ -55,9 +51,19 @@ where
     }
 
     pub fn receive_stuff(&self) {
-        for _ in 0..self.num_messages {
-            let received = self.receive_channel.recv().unwrap();
-            println!("Just received {received:?}.");
+        let mut num_inside = 0;
+        let mut total_points = 0;
+
+        for i in 0..NUM_POINTS {
+            let point: Point = self.receive_channel.recv().unwrap();
+            if point.inside_unit_circle() {
+                num_inside += 1;
+            }
+            total_points += 1;
+
+            if i % 10_000 == 0 {
+                print_estimate(num_inside, total_points);
+            }
         }
     }
 }
@@ -65,20 +71,22 @@ where
 const NUM_POINTS: u32 = 10_000_000;
 
 fn main() {
-    let mut num_inside = 0;
-    let mut total_points = 0;
+    let (send_channel, receive_channel) = sync_channel::<Point>(1_000);
 
-    for i in 0..NUM_POINTS {
-        let point: Point = rng().random();
-        if point.inside_unit_circle() {
-            num_inside += 1;
-        }
-        total_points += 1;
+    let sender_thread = thread::spawn(move || {
+        let sender = MessageGenerator::new(NUM_POINTS, send_channel);
+        sender.send_messages();
+    });
 
-        if i % 10_000 == 0 {
-            print_estimate(num_inside, total_points);
-        }
-    }
+    let receiver_thread = thread::spawn(move || {
+        let receiver = PointManager::new(NUM_POINTS, receive_channel);
+        receiver.receive_stuff();
+    });
+
+    sender_thread.join().unwrap();
+    receiver_thread.join().unwrap();
+
+    println!("All done!")
 }
 
 fn print_estimate(num_inside: u32, total_points: u32) {
@@ -87,22 +95,18 @@ fn print_estimate(num_inside: u32, total_points: u32) {
 }
 
 // fn main() {
-//     const NUM_MESSAGES: u32 = 10;
+//     let mut num_inside = 0;
+//     let mut total_points = 0;
 
-//     let (send_channel, receive_channel) = sync_channel::<Point>(3);
+//     for i in 0..NUM_POINTS {
+//         let point: Point = rng().random();
+//         if point.inside_unit_circle() {
+//             num_inside += 1;
+//         }
+//         total_points += 1;
 
-//     let sender_thread = thread::spawn(move || {
-//         let sender = Sender::new(NUM_MESSAGES, send_channel);
-//         sender.send_stuff();
-//     });
-
-//     let receiver_thread = thread::spawn(move || {
-//         let receiver = Receiver::new(NUM_MESSAGES, receive_channel);
-//         receiver.receive_stuff();
-//     });
-
-//     sender_thread.join().unwrap();
-//     receiver_thread.join().unwrap();
-
-//     println!("All done!")
+//         if i % 10_000 == 0 {
+//             print_estimate(num_inside, total_points);
+//         }
+//     }
 // }
